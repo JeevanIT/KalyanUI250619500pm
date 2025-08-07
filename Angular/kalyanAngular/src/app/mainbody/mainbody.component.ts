041fde978ba5c293aff90ed4898f4e4fe5ff83d2
JeevanIT <jeevanitinfo@gmail.com>
2025-08-07 17:55:36 +0530

    InstallAngular

---
+++ Angular/kalyanAngular/src/app/mainbody/mainbody.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainbody',
+  standalone: true,
+  imports: [],
+  templateUrl: './mainbody.component.html',
+  styleUrl: './mainbody.component.css'
+})
+export class MainbodyComponent {
+
+}
